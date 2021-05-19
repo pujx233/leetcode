300fcd13e44fea7dddec34a0ca82e68cbd0aa03a
pujx233 <1075885060@qq.com>
2021-05-19 13:26:24 +0800

    5.19

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 ## 每日一题
 
 #### [21. 合并两个有序链表](https://leetcode-cn.com/problems/merge-two-sorted-lists/)
+
+#### [83. 删除排序链表中的重复元素](https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/)
---
+++ removeDuplicatesFromSortedList.java
@@ -0,0 +1,48 @@
+/*存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+
+返回同样按升序排列的结果链表。*/
+
+public class removeDuplicatesFromSortedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /*方法一*/
+
+    public class Solution_1 {
+        public ListNode deleteDuplicates(ListNode head) {
+            if(head == null){
+                return null;
+            }
+            ListNode cur = head;
+            while (cur.next!=null){
+                if(cur.next.val == cur.val){
+                    cur.next = cur.next.next;
+                }else {
+                    cur = cur.next;
+                }
+            }
+
+            return head;
+        }
+
+    }
+
+
+
+
+}
